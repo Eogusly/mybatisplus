def662723983df17dfb916377188d32c0b43f1cb
Eogusly <17326066363@163.com>
2020-08-13 18:03:44 +0800

    修改了user，增加了注释

--- src/main/java/com/hb/pojo/User.java
+++ src/main/java/com/hb/pojo/User.java
@@ -17,6 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    //haha
     @TableId(type = IdType.INPUT)
     private Long id;
     private String name;
